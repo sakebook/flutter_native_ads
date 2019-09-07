ef0859dcd695107b3d1a9ea26406c5fddf72f48b
sakebook <sakebook@gmail.com>
2019-09-07 14:34:09 +0900

    Fix invoke method on Android

--- lib/native_ads.dart
+++ lib/native_ads.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 
 class NativeAds {
   NativeAds.initialize() {
-    _channel.invokeListMethod<void>('initialize');
+    _channel.invokeMethod<void>('initialize');
   }
   static const MethodChannel _channel = MethodChannel('native_ads');
 }
